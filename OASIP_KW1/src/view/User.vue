0ab7c11af2aeaaf903bb16f9eea3b0ec038580f1
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2022-08-19 13:48:04 +0700

    make more detail page

---
+++ OASIP_KW1/src/view/User.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref ,onBeforeMount} from "vue";
+import { useRoute, useRouter } from "vue-router";
+const { params } = useRoute();
+
+const userCheck=ref(undefined)
+const userList=ref([])
+const id = params.id;
+
+const userLink=`http://localhost:8080/api/users`
+
+//GET user
+const getUser = async () => {
+  const res = await fetch(userLink);
+  if (res.status === 200) {
+    userList.value = await res.json();
+    userCheck.value = true;
+    console.log(userList.value)
+  } else {
+    userCheck = false;
+  }
+};
+onBeforeMount(async()=>{
+    await getUser();
+})
+</script>
+ 
+<template>
+    <div
+    class="showUp w-5/6 p-5 pb-7 mx-auto mt-10 bg-white rounded-md shadow-xl overflow-auto">
+    testings
+{{id}}
+    </div>
+
+</template>
+ 
+<style>
+
+</style>
