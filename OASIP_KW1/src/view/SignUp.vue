a4407e07a3a9af5195fd8b5af3f410b68d0f1aed
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2022-08-30 00:29:49 +0700

    Update SignUp.vue

--- OASIP_KW1/src/view/SignUp.vue
+++ OASIP_KW1/src/view/SignUp.vue
@@ -7,10 +7,11 @@
   const role = ref('')
   const nameL = 100
   const eMailL = 50
-  const passwordL = 14
   const userList = ref([])
   const passwordd = ref('')
   const passwordC = ref('')
+  const pwMaxL= 14
+  const pwMinL = 8
   //router
   const myRouter = useRouter();
   const goHome = () =>
@@ -77,6 +78,7 @@
       checkEMailN.value = false
       console.log("email is null")
     } else checkEMailN.value = true
+
     // check password null
     if (passwordC.value.length == 0 || passwordd.value.length == 0) {
       checkPasswordN.value = false
@@ -88,21 +90,25 @@
       console.log("name over 100")
       checkNameL.value = false
     } else checkNameL.value = true //;console.log('checkName Length',checkNameL.value)
+
     //check e-mail length
     if (eMail.value.length > eMailL) {
       console.log("email over 50")
       checkEMailL.value = false
     } else checkEMailL.value = true //;console.log('checkEMail Length',checkEMailL.value)
+
     // check e-mail form
     if (valFormEmail(eMail.value) == false) {
       console.log("email invalid form")
       checkEmailF.value = false
     } else checkEmailF.value = true
+
     // check password is length ?
-    if (passwordC.value.length > passwordL || passwordd.value.length > passwordL) {
+    if (passwordd.value.length > pwMaxL || passwordd.value.length < pwMinL) {
       checkPasswordL.value = false
-      console.log("Passwords is over 14 ")
+      console.log("The characters should be at least 8-14 characters. ")
     } else checkPasswordL.value = true
+
     // check unique
     if (isUniqueName.value == true) {
       console.log("name is ununique 😏")
@@ -144,6 +150,8 @@
       name.value = ''
       eMail.value = ''
       role.value = ''
+      passwordC.value=''
+      passwordd.value=''
       goHome()
     } else {
       console.log("can not add new user pls try again")
@@ -180,8 +188,80 @@
   }
   </script>
    
-  <template>
-    <!-- for submit  -->
+  <template>      
+        <div class="w-fit mx-auto 2xl:w-3/4 xl:w9/12 flex">
+          <!-- Col -->
+          <!-- <div class="w-500 h-auto bg-gray-400 hidden  lg:block 2xl:w-1/2 bg-cover rounded-l-lg"
+            style="background-image: url('https://source.unsplash.com/K4mSJ7kc0As/600x800')"></div> -->
+          <!-- Col -->
+          <div class="w-fit  mx-auto lg:w-1/2 bg-white p-5 rounded">
+            <h3 class="pt-2 text-2xl font-semibold text-gray-600 text-center">Register</h3>
+            <form class="px-8 pt-4 pb-4 mb-1 bg-white rounded">
+              <div class="mt-1">
+                <label for="name" class="inline-block mr-1.5 mb-2 text-sm font-bold text-gray-500"> Username </label> 
+                <span class="text-xs"> {{name.length}}/{{nameL}} character</span>
+                <input type="text" id="name" class="bg-gray-500 border border-gray-300 text-gray-900 
+                        sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 
+                        dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                        dark:focus:ring-blue-500 dark:focus:border-blue-500"  required=""
+                  v-model="name">
+              </div>
+              <div class="mt-1">
+                <label for="email" class="inline-block mr-1.5 mb-2 text-sm font-bold text-gray-500">Your email</label>
+                <span class="text-xs">{{eMail.length}}/{{eMailL}} character</span>
+                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 
+                        sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 
+                        dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                        dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="oasip@sit.kmutt.ac.th"
+                  required="" v-model="eMail">
+              </div>
+              <div class="mt-1">
+                <label for="role" class="block mb-2 text-sm font-bold text-gray-500">Role</label>
+                <select id="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+              focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+              dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="role">
+                  <option class="text-gray-500" value="" disabled selected>select your role ( student )</option>
+                  <option class="bg-rose-500" value="student">Student</option>
+                  <option value="lecturer">Lecturer</option>
+                  <option value="admin">Admin</option>
+                </select>
+              </div>
+              <div class="mt-1">
+                <label for="pw" class="inline-block mr-1.5 mb-2 text-sm font-bold text-gray-500">Password</label>
+                <span class="text-xs">{{passwordd.length}}/{{pwMaxL}} character</span>
+                <span v-if="passwordd.length>14 || passwordd.length<8" class="text-rose-400 ml-1 text-xs">* The characters should be at least 8-14 characters. *</span>
+                <input type="password" name="password" id="pw"  class="bg-gray-50 border 
+                        border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 
+                        block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
+                        dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""
+                  v-model="passwordd">
+              </div>
+              <div class="mt-1">
+                <label for="cpw" class="inline-block mb-2 text-sm font-bold text-gray-500 ">Confirm password</label> 
+                <span v-if="noMatchP==true" class="text-xs text-rose-400">* password not match *</span>
+                <span></span>
+                <input type="password" name="confirm-password" id="cpw"  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 
+                        focus:border-primary-600 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
+                        dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""
+                  v-model="passwordC">
+              </div>
+                <a href="#submit" >
+                  <div class="mt-4 w-full px-4 py-2 font-bold text-center text-white bg-rose-400 rounded-full hover:bg-rose-500 focus:outline-none focus:shadow-outline">
+                   Sign In 
+                 </div>
+                </a>
+              
+              <!-- <div class="text-center">
+                <a class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800" href="./register.html">Create an Account!</a>
+              </div>
+              <div class="text-center">
+                <a class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800" href="./forgot-password.html">Forgot Password?</a>
+              </div> -->
+            </form>
+          </div>
+        </div>
+
+      <!-- for submit  -->
     <div id="submit" class="overlay">
       <div class="popup2 ">
         <h2 class="mb-5 text-xl font-bold bg-white mx-auto w-fit">Are you sure ?</h2>
@@ -192,73 +272,23 @@
         </div>
       </div>
     </div>
-  
-      
-        <div class="w-fit mx-auto 2xl:w-3/4 xl:w9/12 flex">
-          <!-- Col -->
-          <div class="w-500 h-auto bg-gray-400 hidden  lg:block 2xl:w-1/2 bg-cover rounded-l-lg"
-            style="background-image: url('https://source.unsplash.com/K4mSJ7kc0As/600x800')"></div>
-          <!-- Col -->
-          <div class="w-full lg:w-1/2 bg-white p-5 rounded-lg lg:rounded-l-none">
-            <h3 class="pt-2 text-2xl text-center">Register</h3>
-            <form class="px-8 pt-4 pb-4 mb-1 bg-white rounded">
-              <div>
-                <label for="name" class="inline-block mr-1.5 mb-2 text-sm font-bold text-gray-700"> Username </label> <span class="text-xs"> {{name.length}}/{{nameL}} character</span>
-                <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 
-                        sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 
-                        dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-                        dark:focus:ring-blue-500 dark:focus:border-blue-500"  required=""
-                  v-model="name">
-              </div>
-              <div>
-                <label for="email" class="block mb-2 text-sm font-bold text-gray-700">Your email</label>
-                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 
-                        sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2 
-                        dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-                        dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="oasip@sit.kmutt.ac.th"
-                  required="" v-model="eMail">
-              </div>
-              <div>
-                <label for="role" class="block mb-2 text-sm font-bold text-gray-700">Role</label>
-                <select id="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
-              focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
-              dark:focus:ring-blue-500 dark:focus:border-blue-500" v-model="role">
-                  <option value="" disabled selected>select your role</option>
-                  <option value="student">Student</option>
-                  <option value="lecturer">Lecturer</option>
-                  <option value="admin">Admin</option>
-                </select>
-              </div>
-              <div>
-                <label for="pw" class="block mb-2 text-sm font-bold text-gray-700">Password</label>
-                <input type="password" name="password" id="pw"  class="bg-gray-50 border 
-                        border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 
-                        block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
-                        dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""
-                  v-model="passwordd">
-              </div>
-              <div>
-                <label for="cpw" class="inline-block mb-2 text-sm font-bold text-gray-700 ">Confirm password</label> <span v-if="noMatchP==true" class="text-xs text-rose-400">* password not match *</span>
-                <input type="password" name="confirm-password" id="cpw"  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 
-                        focus:border-primary-600 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 
-                        dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""
-                  v-model="passwordC">
-              </div>
-              <br>
-                  <div class="w-full px-4 py-2 font-bold text-center text-white bg-blue-500 rounded-full hover:bg-blue-700 focus:outline-none focus:shadow-outline">
-                  <a href="#submit" class="p-6 m-2"> Sign In </a>
-                 </div>
-              <hr class="mb-6 border-t" />
-              <!-- <div class="text-center">
-                <a class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800" href="./register.html">Create an Account!</a>
-              </div>
-              <div class="text-center">
-                <a class="inline-block text-sm text-blue-500 align-baseline hover:text-blue-800" href="./forgot-password.html">Forgot Password?</a>
-              </div> -->
-            </form>
-          </div>
+
+    <!-- const checkNameN = ref(undefined)
+  const checkEMailN = ref(undefined)
+  const checkNameL = ref(undefined)
+  const checkEMailL = ref(undefined)
+  const checkEmailF = ref(undefined)
+  const checkPasswordN = ref(undefined)
+  const checkPasswordL = ref(undefined)
+  const noMatchP =ref(undefined) -->
+    <div class="alert-area">
+      <div v-if="true== true" class="alert warning text-sm">
+          <span class="closebtn" @click="checkEMailN = false">x</span>
+          <strong class="block">Warning!</strong> Please enter your email.
         </div>
-  
+    </div>
+
+
   </template>
    
   <style scoped>
@@ -312,4 +342,79 @@
       width: 20%;
     }
   }
+
+  
+.showUp {
+  position: relative;
+  animation: wii 1s;
+  animation-timing-function: ease-in-out;
+}
+
+/* alert */
+.alert-area {
+  position: fixed;
+  top: 150px;
+  right: 0;
+  background-color: gray;
+  width: 18%;
+  margin-right: 10px;
+}
+
+.alert {
+  position: relative;
+  width: 100%;
+  padding-top: 15px;
+  padding-left: 15px;
+  padding-right: 15px;
+  padding-bottom: 10px;
+  animation: moveLeft 0.9s;
+  animation-timing-function: ease-in-out;
+  background-color: #f44336;
+  color: white;
+  opacity: 1;
+  transition: opacity 0.6s;
+  margin-bottom: 15px;
+}
+
+.alert.success {
+  background-color: #d547e4;
+}
+
+.alert.info {
+  background-color: #2196f3;
+}
+
+.alert.warning {
+  background-color: #ff9800;
+}
+
+.closebtn {
+  margin-left: 15px;
+  color: white;
+  font-weight: bold;
+  float: right;
+  font-size: 22px;
+  line-height: 20px;
+  cursor: pointer;
+  transition: 0.3s;
+}
+
+.closebtn:hover {
+  color: black;
+}
+
+/* animation */
+@keyframes moveLeft {
+  0% {
+    right: -300px;
+    top: 0px;
+    opacity: 0;
+  }
+
+  100% {
+    right: 0px;
+    top: 0px;
+    opacity: 1.5;
+  }
+}
   </style>
