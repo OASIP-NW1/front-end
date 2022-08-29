0de3928e649d2021527cc52d81442f440ad2b9aa
Zeed <Phiraphat.zim@mail.kmutt.ac.th>
2022-08-29 22:19:51 +0700

    Update App.vue

--- OASIP_KW1/src/App.vue
+++ OASIP_KW1/src/App.vue
@@ -20,65 +20,66 @@ const goSignIn = () => myRouter.push({ name: 'SignIn' })
 
 <template>
     <div id="bg_color">
-        <nav class="relative px-5 py-7 flex justify-center items-center bg-transparent drop-shadow-md md:rounded-lg md:mx-auto w-full">
+        <nav class="relative px-5 mb-5 py-2 flex justify-center items-center bg-gray-400 drop-shadow-md md:rounded-b-lg md:mx-auto w-fit">
             <img src="./assets/icon.png" alt="LOGO" :style="logoSize" />
+           
             <span class="font-bold text-xl text-white mx-4">
                 <BaseDate :isTime='true' />
 
                 <br>
-                <span class=" font-medium">
+                <span class=" font-medium ">
                     <BaseDate :isDate="true" />
                 </span>
             </span>
-            <ul class="hidden absolute lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
-                <li>
+            
+            <div class=" flex mx-10 lg:flex lg:items-center lg:w-auto lg:space-x-6">
+                                <div>
                             <button type="button" class="inline-block px-2 py-2 bg-transparent text-white 
                             font-medium text-lg leading-tight  rounded  
                             hover:bg-gray-900 hover:shadow-lg focus:bg-blue-400 focus:shadow-lg 
                             focus:outline-none focus:ring-0 active:bg-blue-300 active:shadow-lg 
                             transition duration-150 ease-in-out " @click="goHome">Home</button>
-                                </li>
-                                <li>
+                                </div>
+                                <div>
                                     <button type="button" class="inline-block px-2 py-2  bg-transparent text-white 
                             font-medium text-lg leading-tight  rounded  
                             hover:bg-gray-900 hover:shadow-lg focus:bg-blue-400 focus:shadow-lg 
                             focus:outline-none focus:ring-0 active:bg-gray-300 active:shadow-lg 
                             transition duration-150 ease-in-out " @click="goCategory">Category</button>
-                                </li>
-                                <li>
+                                </div>
+                                <div>
                                     <button type="button" class="inline-block px-4 py-2 bg-transparent text-white 
                             font-medium text-lg leading-tight  rounded 
                              hover:bg-gray-900 hover:shadow-lg focus:bg-blue-400
                             focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-300
                             active:shadow-lg transition duration-150 ease-in-out" @click="goBooking">Booking</button>
 
-                                </li>
-                                <li>
+                                </div>
+                                <div>
                             <button type="button" class="inline-block px-2 py-2  bg-transparent text-white 
                             font-medium text-lg leading-tight  rounded  
                             hover:bg-gray-900 hover:shadow-lg focus:bg-blue-400 focus:shadow-lg 
                             focus:outline-none focus:ring-0 active:bg-gray-300 active:shadow-lg 
                             transition duration-150 ease-in-out " @click="goReservationList">Reservation</button>
-                                </li>
-
-                                <li>
+                                </div>
+                                <div>
                             <button type="button" class="inline-block px-2 py-2  bg-transparent text-white 
                             font-medium text-lg leading-tight  rounded 
                             hover:bg-gray-900 hover:shadow-lg focus:bg-blue-400 focus:shadow-lg 
                             focus:outline-none focus:ring-0 active:bg-gray-300 active:shadow-lg 
                             transition duration-150 ease-in-out " @click="goAllUser">Show all user</button>
-                                </li>
-                                <li>
+                                </div>
+                                <div>
                             <button type="button" class="inline-block px-2 py-2  bg-transparent text-white 
                             font-medium text-lg leading-tight  rounded 
                             hover:bg-gray-900 hover:shadow-lg focus:bg-blue-400 focus:shadow-lg 
                             focus:outline-none focus:ring-0 active:bg-gray-300 active:shadow-lg 
                             transition duration-150 ease-in-out " @click="goContactUs">Contact us</button>
-                                </li>
-            </ul>
-            <a class="hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-blue-500 hover:bg-blue-600 text-l text-white 
-        font-bold  rounded-xl transition duration-200" href="#" @click="goSignIn">Sign In / Log In</a>
-            <a class="hidden lg:inline-block lg:ml-right py-2 px-6 bg-emerald-500 hover:bg-emerald-600 text-l text-white 
+                                </div>
+            </div>
+            <button class="hidden lg:inline-block mx-2 py-2 px-6 bg-blue-500 hover:bg-blue-600 text-l text-white 
+        font-bold  rounded-xl transition duration-200"  @click="goSignIn">Sign In</button>
+            <a class="hidden lg:inline-block mx-2 py-2 px-6 bg-emerald-500 hover:bg-emerald-600 text-l text-white 
         font-bold rounded-xl " href="#" @click="goSignUp">Register</a>
         </nav>
         <div class="flex object-cover ">
@@ -91,7 +92,7 @@ const goSignIn = () => myRouter.push({ name: 'SignIn' })
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap');
 
-ul li {
+/* ul li {
     list-style: none;
     margin: 0 auto;
     display: inline-block;
@@ -100,7 +101,7 @@ ul li {
     text-decoration: none;
     text-align: center;
     font-family: 'Inter', sans-serif;
-}
+} */
 
 #bg_color {
     /* background-color: rgb(252 165 165); */
